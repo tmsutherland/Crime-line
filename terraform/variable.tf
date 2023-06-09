66f6d5bb973ea14b9604fdcef65ad2644e6fa87e
tmsutherland <60674380+tmsutherland@users.noreply.github.com>
2023-06-09 14:17:00 -0700

    Update variable.tf
    update aws region to west

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -2,7 +2,7 @@
 variable "aws_region" {
   description = "AWS region"
   type        = string
-  default     = "us-east-1"
+  default     = "us-west-1"
 }
 
 ## Key to allow connection to our EC2 instance
